aae702af386551f96ea562c59a60153057482ec4
sccala <sccala18@gmail.com>
2021-07-30 09:43:50 -0700

    add:added material ui

--- src/App.js
+++ src/App.js
@@ -1,10 +1,49 @@
+import './App.css'
+import { Typography, AppBar, Card, CardActions, CardContent, CardMedia, CssBaseline, Toolbar, Grid, Container, Button } from '@material-ui/core'
+import { PhotoCamera } from '@material-ui/icons'
 
-import './App.css';
+import useStyles from './styles'
 
 function App() {
+  const classes = useStyles()
   return (
-   <div>Hello this is a card game for Mintbean assignment</div>
-  );
+    <>
+      <CssBaseline />
+      <AppBar position="relative">
+        <Toolbar>
+          <PhotoCamera className={classes.icon} />
+          <Typography variant="h6">Photo Album</Typography>
+        </Toolbar>
+      </AppBar>
+      <main>
+        <div className={classes.container}>
+          <Container maxWidth="sm">
+            <Typography variant="h2" align="center" color="textPrimary" gutterBottom>
+              Photo Album
+            </Typography>
+            <Typography variant="h5" align="center" color="textSeCondary" paragraph>
+              Hello everyone this is a photo album and I am trying to make this sentence as long as possible so it can describe how it will look like
+              on the screen
+            </Typography>
+            <div className={classes.buttons}>
+              <Grid container spacing={2} justify="center">
+                <Grid item>
+                  <Button variant="contained" color="primary">
+                    See my photos
+                  </Button>
+                </Grid>
+                <Grid item>
+                  <Button variant="outlined" color="primary">
+                    Secondary buttons
+                  </Button>
+                </Grid>
+              </Grid>
+            </div>
+          </Container>
+        </div>
+      </main>
+    </>
+  )
 }
 
-export default App;
+export default App
